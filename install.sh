0eb47d9db86003bcdda4ecca173a2c62513f10b7
Sanchit Saini <sanchitsaini14@gmail.com>
2021-10-29 20:58:27 +0530

    Install: Add installer script

---
+++ install.sh
@@ -0,0 +1,11 @@
+if [ ! -d "st" ]; then
+    git clone https://github.com/torrinfail/dwmblocks
+    cd dwmblocks
+else
+    cd dwmblocks && git restore .
+    rm -f blocks.h && make clean
+fi
+
+git apply ../patches/*.txt
+
+sudo make install
